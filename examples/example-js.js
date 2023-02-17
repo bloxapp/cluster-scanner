88e6206a2b16f37bef0b5015d6d2e40a6a2820f4
AndrewBlox <andrew@blox.io>
2023-02-18 01:49:27 +0200

    update examples

--- examples/example-js.js
+++ examples/example-js.js
@@ -2,10 +2,10 @@ const { SSVScannerCommand } = require('../dist/tsc/src/commands/SSVScannerComman
 
 async function main() {
   const params = {
-    nodeUrl: 'https://eth-goerli.alchemyapi.io/v2/G3aG8wN9V8jKWs0NwZkWf2-img3cH4CU',
-    contractAddress: '0x3F55b8642157046F095173e3F7D86bfF2D1B52D2',
-    ownerAddress: '0xb2AeBd073B24De663e685717d26d089Fdaa44444',
-    operatorIds: [1, 2, 3, 4],
+    nodeUrl: '',
+    contractAddress: '',
+    ownerAddress: '',
+    operatorIds: [],
   }
   const command = new SSVScannerCommand(params);
   const result = await command.scan()
